105fcb4cca219e0d8fc43223df9659ca9cf7e5d2
Marcos Ramírez <marcospha@gmail.com>
2015-04-22 19:32:23 -0430

    Integración del módulo de gestión de usuarios.

--- common/config/main.php
+++ common/config/main.php
@@ -29,5 +29,27 @@ return [
 		'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
         ],
+	    'user' => [
+	        'class' => 'webvimark\modules\UserManagement\components\UserConfig',
+
+	        // Comment this if you don't want to record user logins
+	        'on afterLogin' => function($event) {
+	                \webvimark\modules\UserManagement\models\UserVisitLog::newVisitor($event->identity->id);
+	            }
+	    ],
     ],
+	'modules'=>[
+	    'user-management' => [
+	        'class' => 'webvimark\modules\UserManagement\UserManagementModule',
+
+	        // Here you can set your handler to change layout for any controller or action
+	        // Tip: you can use this event in any module
+	        'on beforeAction'=>function(yii\base\ActionEvent $event) {
+	                if ( $event->action->uniqueId == 'user-management/auth/login' )
+	                {
+	                    $event->action->controller->layout = 'loginLayout.php';
+	                };
+	            },
+	    ],
+	],
 ];
--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -78,7 +78,33 @@ AppAsset::register($this);
         <?= $content ?>
         </div>
     </div>
+<?php 
 
+use webvimark\modules\UserManagement\components\GhostMenu;
+use webvimark\modules\UserManagement\UserManagementModule;
+
+echo GhostMenu::widget([
+    'encodeLabels'=>false,
+    'activateParents'=>true,
+    'items' => [
+        [
+            'label' => 'Backend routes',
+            'items'=>UserManagementModule::menuItems()
+        ],
+        [
+            'label' => 'Frontend routes',
+            'items'=>[
+                ['label'=>'Login', 'url'=>['/user-management/auth/login']],
+                ['label'=>'Logout', 'url'=>['/user-management/auth/logout']],
+                ['label'=>'Registration', 'url'=>['/user-management/auth/registration']],
+                ['label'=>'Change own password', 'url'=>['/user-management/auth/change-own-password']],
+                ['label'=>'Password recovery', 'url'=>['/user-management/auth/password-recovery']],
+                ['label'=>'E-mail confirmation', 'url'=>['/user-management/auth/confirm-email']],
+            ],
+        ],
+    ],
+]);
+?>
     <footer class="footer">
         <div class="container">
         <p class="pull-left">&copy; Registro Nacional de Contratistas <?= date('Y') ?></p>
@@ -90,3 +116,4 @@ AppAsset::register($this);
 </body>
 </html>
 <?php $this->endPage() ?>
+
