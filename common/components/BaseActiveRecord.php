14f8350bb5ad7f8b785efe71f7371c9e958dcafe
Edgar Leal <edgar.leal0@gmail.com>
2015-05-13 23:16:38 -0430

    Css, vista cuenta A, esqueleto para colocar los metodos y tecnicas en el modelo base, funciones dentro de SysTotales

--- common/components/BaseActiveRecord.php
+++ common/components/BaseActiveRecord.php
@@ -168,4 +168,70 @@ class BaseActiveRecord extends ActiveRecord
             return null;
     }*/
 
+/*+++++++++++++++++++++++++++++++METODOS DISPONIBLES PARA HACER LOS CALCULOS EN EL SISTEMA++++++++++++++++++++++++++++*/
+
+    /**
+     * @return bool
+     */
+    public function getPromedio()
+    {
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getPorCapas()
+    {
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getDelCosto()
+    {
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getLineal()
+    {
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getDecreciente()
+    {
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getBasadoUso()
+    {
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getVarlorRazonable()
+    {
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getRevaluacion()
+    {
+        return true;
+    }
+
 }
--- frontend/views/a-efectivos-bancos/efectivosequivalentes.php
+++ frontend/views/a-efectivos-bancos/efectivosequivalentes.php
@@ -8,6 +8,9 @@ use kartik\widgets\ActiveForm;
 use kartik\builder\TabularForm;
 use kartik\grid\GridView;
 use kartik\popover\PopoverX;
+use app\models\AEfectivosBancosSearch;
+use common\models\c\SysTotales;
+
 //use kartik\helpers\Html;
 
 
@@ -21,7 +24,8 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="aefectivos-bancos-create">
 
     <center><h1><?= Html::encode($this->title) ?></h1>
-
+      <br/>
+   
    <?php
    			/*
                 $menuItems[] =  ['label'=>'Efectivos en bancos', 'url'=>['/a-efectivos-bancos/create']];
@@ -40,32 +44,6 @@ $this->params['breadcrumbs'][] = $this->title;
           */
         ?>
         </center>
-        <div class="container">
-         
-        </div>
-
-
-<?php
- /*$form = ActiveForm::begin(['fieldConfig'=>['showLabels'=>false]]);
- $attribs = $model->getFormAttribs('nacional');
- echo TabularForm::widget([
-    'dataProvider'=>$dataProvider,
-    'form'=>$form,
-    'attributes'=>$attribs,
-    'gridSettings'=>[
-        'floatHeader'=>false,
-        'panel'=>[
-            'heading' => '<h3 class="panel-title"><i class="glyphicon glyphicon-book"></i> Gestionar Bancos</h3>',
-            'type' => GridView::TYPE_PRIMARY,
-            'after'=> Html::a('<i class="glyphicon glyphicon-remove"></i> Delete', '#', ['class'=>'btn btn-danger']) . ' ' .
-                    Html::submitButton('<i class="glyphicon glyphicon-floppy-disk"></i> Save', ['class'=>'btn btn-primary'])
-        ]
-    ]
-]);
-
-
-ActiveForm::end();*/
-?>
     <?php
     $columns = [
         ['class'=>'kartik\grid\SerialColumn', 'order'=>DynaGrid::ORDER_FIX_LEFT],
@@ -119,7 +97,6 @@ ActiveForm::end();*/
         ],
         ['class'=>'kartik\grid\CheckboxColumn', 'order'=>DynaGrid::ORDER_FIX_RIGHT],
     ];
-    use app\models\AEfectivosBancosSearch;
     echo DynaGrid::widget([
         'columns'=>$columns,
         'storage'=>DynaGrid::TYPE_COOKIE,
@@ -127,10 +104,177 @@ ActiveForm::end();*/
         'gridOptions'=>[
             'dataProvider'=>$dataProvider,
             //'filterModel'=>AEfectivosBancosSearch,
-            'panel'=>['heading'=>'<h3 class="panel-title">Efectivo en Bancos Nacionales</h3>'],
+            'panel'=>['heading'=>'<h3 class="panel-title">Efectivo en caja</h3>'],
         ],
         'options'=>['id'=>'dynagrid-1'] // a unique identifier is important
     ]);
 
     ?>
-</div>
+
+
+    <?php
+    $columns = [
+        ['class'=>'kartik\grid\SerialColumn', 'order'=>DynaGrid::ORDER_FIX_LEFT],
+        //'id',
+        [
+          'attribute' => 'banco_contratista_id',
+          'label' => 'Banco',
+          'format' => 'html',
+          'value' => function ($model)
+          {
+              return '<div>'.$model->bancoContratista->banco->nombre.'</div>';
+          }
+        ],
+        'saldo_segun_b',
+        'nd_no_cont',
+        'depo_transito',
+        'nc_no_cont',
+        'cheques_transito',
+        'saldo_al_cierre',
+        'intereses_act_eco',
+        [
+          'attribute' => 'tipo_moneda_id',
+          'label' => 'Tipo moneda',
+          'format' => 'html',
+          'value' => function ($model)
+          {
+              return '<div>'.$model->tipoMoneda->nombre.'</div>';
+          }
+        ],
+        
+        'monto_moneda_extra',
+        'tipo_cambio_cierre',
+/*        [
+            'attribute'=>'nd_no_cont',
+            'filterType'=>GridView::FILTER_DATE,
+            'format'=>'raw',
+            'width'=>'170px',
+            'filterWidgetOptions'=>[
+                'pluginOptions'=>['format'=>'yyyy-mm-dd']
+            ],
+        ],
+        [
+            'class'=>'kartik\grid\BooleanColumn',
+            'attribute'=>'depo_transito',
+            'vAlign'=>'middle',
+        ],*/
+        [
+            'class'=>'kartik\grid\ActionColumn',
+            'dropdown'=>false,
+            'order'=>DynaGrid::ORDER_FIX_RIGHT
+        ],
+        ['class'=>'kartik\grid\CheckboxColumn', 'order'=>DynaGrid::ORDER_FIX_RIGHT],
+    ];
+    //use app\models\AEfectivosBancosSearch;
+    echo DynaGrid::widget([
+        'columns'=>$columns,
+        'storage'=>DynaGrid::TYPE_COOKIE,
+        'theme'=>'panel-danger',
+        'gridOptions'=>[
+            'dataProvider'=>$dataProvider,
+            //'filterModel'=>AEfectivosBancosSearch,
+            'panel'=>['heading'=>'<h3 class="panel-title">Efectivo en bancos</h3>'],
+        ],
+        'options'=>['id'=>'dynagrid-2'] // a unique identifier is important
+    ]);
+
+    ?>
+
+    <?php
+    $columns = [
+        ['class'=>'kartik\grid\SerialColumn', 'order'=>DynaGrid::ORDER_FIX_LEFT],
+        //'id',
+        [
+          'attribute' => 'banco_contratista_id',
+          'label' => 'Banco',
+          'format' => 'html',
+          'value' => function ($model)
+          {
+              return '<div>'.$model->bancoContratista->banco->nombre.'</div>';
+          }
+        ],
+        'saldo_segun_b',
+        'nd_no_cont',
+        'depo_transito',
+        'nc_no_cont',
+        'cheques_transito',
+        'saldo_al_cierre',
+        'intereses_act_eco',
+        [
+          'attribute' => 'tipo_moneda_id',
+          'label' => 'Tipo moneda',
+          'format' => 'html',
+          'value' => function ($model)
+          {
+              return '<div>'.$model->tipoMoneda->nombre.'</div>';
+          }
+        ],
+        
+        'monto_moneda_extra',
+        'tipo_cambio_cierre',
+/*        [
+            'attribute'=>'nd_no_cont',
+            'filterType'=>GridView::FILTER_DATE,
+            'format'=>'raw',
+            'width'=>'170px',
+            'filterWidgetOptions'=>[
+                'pluginOptions'=>['format'=>'yyyy-mm-dd']
+            ],
+        ],
+        [
+            'class'=>'kartik\grid\BooleanColumn',
+            'attribute'=>'depo_transito',
+            'vAlign'=>'middle',
+        ],*/
+        [
+            'class'=>'kartik\grid\ActionColumn',
+            'dropdown'=>false,
+            'order'=>DynaGrid::ORDER_FIX_RIGHT
+        ],
+        ['class'=>'kartik\grid\CheckboxColumn', 'order'=>DynaGrid::ORDER_FIX_RIGHT],
+    ];
+    //use app\models\AEfectivosBancosSearch;
+    echo DynaGrid::widget([
+        'columns'=>$columns,
+        'storage'=>DynaGrid::TYPE_COOKIE,
+        'theme'=>'panel-danger',
+        'gridOptions'=>[
+            'dataProvider'=>$dataProvider,
+            //'filterModel'=>AEfectivosBancosSearch,
+            'panel'=>['heading'=>'<h3 class="panel-title">Inversiones para negociar</h3>'],
+        ],
+        'options'=>['id'=>'dynagrid-3'] // a unique identifier is important
+    ]);
+
+    ?>
+    <div> 
+        <table class="table table-bordered">
+            <caption>Total efectivo y sus equivalentes</caption>
+            <thead>
+              <tr>
+                <th>
+                    Saldo al cierre de la actividad económica
+                </th>
+                <th>
+                    Intereses generados durante actividad económica
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                    Probando
+                </td>
+                 <td>
+                    Probando
+                </td>
+              </tr>
+            </tbody>
+        </table>
+    </div>
+    <?php
+        $model = new SysTotales();
+        $total = $model->getTotales();
+        //print_r($total); 
+    ?>
+</div>  
