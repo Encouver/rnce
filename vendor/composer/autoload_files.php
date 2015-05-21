bc2da756fdd2c4ea1011f8ce8d9c8155efe24912
Edgar Leal <edgar.leal0@gmail.com>
2015-05-21 18:10:13 -0430

    Edgarleal composer.json

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,6 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    $vendorDir . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
     $vendorDir . '/swiftmailer/swiftmailer/lib/swift_required.php',
 );
