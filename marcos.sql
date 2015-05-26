1a2710336c91d4e5f73d31407ba5337a032be46c
Marcos Ramírez <marcospha@gmail.com>
2015-05-26 19:28:08 -0430

    Backup de la BD en marcos-rnc.backup Cambios en script en marcos.sql.

--- marcos.sql
+++ marcos.sql
@@ -1413,3 +1413,33 @@ COMMENT ON COLUMN activos.bienes.perdida_reverso IS 'Perdida o reverso.';
 
 
 INSERT INTO activos.facturas (id, num_factura, proveedor_id, fecha_emision, imprenta_id, fecha_emision_talonario, comprador_id, base_imponible_gravable, exento, iva, contratista_id, creado_por, actualizado_por, sys_status, sys_creado_el, sys_actualizado_el, sys_finalizado_el) VALUES (2, '1', 1, '2015-12-12', 2, '2015-12-12', 1, 65456.000000, 456.000000, 56.000000, 1, NULL, NULL, true, '2015-05-25 10:16:18.736-04:30', '2015-05-25 10:16:18.736-04:30', NULL);
+
+
+
+
+
+/**************     26/05/2015 *************/
+
+
+-- Column: contratista_id
+
+ALTER TABLE nombres_cajas DROP COLUMN IF EXISTS contratista_id;
+
+ALTER TABLE nombres_cajas ADD COLUMN contratista_id integer;
+ALTER TABLE nombres_cajas ALTER COLUMN contratista_id SET NOT NULL;
+COMMENT ON COLUMN nombres_cajas.contratista_id IS 'Clave foranea al contratista';
+
+ALTER TABLE nombres_cajas
+ADD FOREIGN KEY (contratista_id) REFERENCES contratistas (id) ON UPDATE CASCADE ON DELETE NO ACTION;
+
+-- Column: anho
+
+ALTER TABLE nombres_cajas DROP COLUMN IF EXISTS anho;
+
+ALTER TABLE nombres_cajas ADD COLUMN anho character varying(100);
+ALTER TABLE nombres_cajas ALTER COLUMN anho SET NOT NULL;
+COMMENT ON COLUMN nombres_cajas.anho IS 'Año contable y mes';
+
+
+ALTER TABLE nombres_cajas
+  DROP COLUMN contratistas_id;
