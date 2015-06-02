991481cbdde93ce7e9afb6c8771c2ae79f601d8c
Marcos Ramírez <marcospha@gmail.com>
2015-06-02 18:34:32 -0430

    Pruebas.

--- frontend/views/cuentas-d1-islr-pagado-anticipo/_form.php
+++ frontend/views/cuentas-d1-islr-pagado-anticipo/_form.php
@@ -35,7 +35,7 @@ use yii\helpers\Html;
         <div class="panel-heading"><h4><i class="glyphicon "></i> Beneficiarios</h4></div>
         <div class="panel-body">
             <?php DynamicFormWidget::begin([
-                'widgetContainer' => 'dynamicform_wrapper', // required: only alphanumeric characters plus "_" [A-Za-z0-9_]
+                'widgetContainer' => 'dynamicform_wrapper2', // required: only alphanumeric characters plus "_" [A-Za-z0-9_]
                 'widgetBody' => '.container-items', // required: css class selector
                 'widgetItem' => '.item', // required: css class
                 'limit' => 4, // the maximum times, an element can be cloned (default 999)
@@ -127,26 +127,26 @@ use yii\helpers\Html;
 
  $script = <<< JS
 
-            $(".dynamicform_wrapper").on("beforeInsert", function(e, item) {
+            $(".dynamicform_wrapper2").on("beforeInsert", function(e, item) {
                 console.log("beforeInsert");
             });
 
-            $(".dynamicform_wrapper").on("afterInsert", function(e, item) {
+            $(".dynamicform_wrapper2").on("afterInsert", function(e, item) {
                 console.log("afterInsert");
             });
 
-            $(".dynamicform_wrapper").on("beforeDelete", function(e, item) {
+            $(".dynamicform_wrapper2").on("beforeDelete", function(e, item) {
                 if (! confirm("Are you sure you want to delete this item?")) {
                     return false;
                 }
                 return true;
             });
 
-            $(".dynamicform_wrapper").on("afterDelete", function(e) {
+            $(".dynamicform_wrapper2").on("afterDelete", function(e) {
                 console.log("Deleted item!");
             });
 
-            $(".dynamicform_wrapper").on("limitReached", function(e, item) {
+            $(".dynamicform_wrapper2").on("limitReached", function(e, item) {
                 alert("Limit reached");
             });
 
