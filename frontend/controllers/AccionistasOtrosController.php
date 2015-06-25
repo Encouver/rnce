d809298e694216ee32c466169f7e0d9d14950200
raul0115 <raul0115@gmail.com>
2015-06-24 20:15:23 -0430

    resolviendo bug en el campo de busqueda

--- frontend/controllers/AccionistasOtrosController.php
+++ frontend/controllers/AccionistasOtrosController.php
@@ -433,14 +433,14 @@ class AccionistasOtrosController extends BaseController
     }
     
      public function actionAccionistasOtrosLista($q = null, $id = null,$sucursal=null) {
-    $buscar_accionista= "natura.denominacion ILIKE "."'%" . $q ."%' and natura.id=accionista.natural_juridica_id and accionista.junta_directiva=false";   
+    $buscar_accionista= "(natura.denominacion ILIKE "."'%" . $q ."%' or natura.rif ILIKE "."'%" . $q ."%') and natura.id=accionista.natural_juridica_id and accionista.junta_directiva=false";   
       //$buscar_accionista= "natura.denominacion ILIKE "."'%" . $q ."%' and natura.id=accionista.natural_juridica_id and accionista.accionista=true";     
     \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
      $out = ['results' => ['id' => '', 'text' => '']];
     if (!is_null($q)) {
         $query = new \yii\db\Query;
        if(!is_null($sucursal)){
-                 $buscar_accionista= "natura.denominacion ILIKE "."'%" . $q ."%' and natura.id=accionista.natural_juridica_id and accionista.accionista=true";   
+                 $buscar_accionista= "(natura.denominacion ILIKE "."'%" . $q ."%' or natura.rif ILIKE "."'%" . $q ."%') and natura.id=accionista.natural_juridica_id and accionista.accionista=true";   
             }
         
         $query->select("natura.id, natura.denominacion AS text")
