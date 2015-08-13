44124265b98dcbe3d0498d9ee39a6d6d059c8c68
Marcos Ramirez <marcospha@gmail.com>
2015-08-13 10:22:20 -0430

    Arreglos en las vistas de comisario auditor.

--- frontend/views/comisarios-auditores/index.php
+++ frontend/views/comisarios-auditores/index.php
@@ -22,7 +22,7 @@ $this->params['breadcrumbs'][] = $this->title;
         //'filterModel' => $searchModel,
         'summary'=>'',
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+            //['class' => 'yii\grid\SerialColumn'],
             [
                 'attribute'=>'natural_juridica_id',
                 'label'=>'Nombre comisario',
